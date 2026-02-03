c5310fe3773ad231cad76a98d2e69637bb3e0b99
Nyazul <ansarinyazul2003@gmail.com>
2026-02-03 23:03:21 +0530

    Solving CORS issue attemp 2

--- backend/src/main/java/com/ysminfosolution/realestate/security/SecurityConfig.java
+++ backend/src/main/java/com/ysminfosolution/realestate/security/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/api/**", configuration);
         return source;
     }
 
