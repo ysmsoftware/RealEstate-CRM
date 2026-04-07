a6f7e45146e6b13ca6d321b4d66d3b9cc7059bd1
Nyazul <ansarinyazul2003@gmail.com>
2026-04-07 12:32:29 +0530

    Trimmed all the Strings in every request

---
+++ backend/src/main/java/com/ysminfosolution/realestate/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+package com.ysminfosolution.realestate.config;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.ysminfosolution.realestate.util.StringTrimmerDeserializer;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Module stringTrimmerModule() {
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(String.class, new StringTrimmerDeserializer());
+        return module;
+    }
+}
